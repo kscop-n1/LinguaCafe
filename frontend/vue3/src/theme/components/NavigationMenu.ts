09565983e2b9b8a7e4ccea9a7349459172ed4df7
simjanos-dev <simjanos.dev@gmail.com>
2025-10-12 19:03:35 +0200

    Vue3: removed tailwind important symbols

--- frontend/vue3/src/theme/components/NavigationMenu.ts
+++ frontend/vue3/src/theme/components/NavigationMenu.ts
@@ -3,18 +3,23 @@ export default {
         link: 'before:rounded-r-full rounded-r-full select-none cursor-pointer',
         linkLabel: 'select-none py-1',
     },
-    // ! symbol used due to compound variants
     variants: {
         active: {
-            true: {
-                link: 'rounded-r-full select-none bg-primary text-inverted!',
-                linkLeadingIcon: 'text-inverted!',
-            },
-
             false: {
                 link: 'text-toned',
                 linkLeadingIcon: 'text-toned',
             },
         },
     },
+    compoundVariants: [
+        {
+            color: 'primary',
+            variant: 'pill',
+            active: true,
+            class: {
+                link: 'rounded-r-full select-none bg-primary text-inverted',
+                linkLeadingIcon: 'text-inverted',
+            },
+        },
+    ],
 }
