c4509d64b62cd4876fb0ebeda61a84f15eafd540
simjanos-dev <simjanos.dev@gmail.com>
2025-10-27 00:01:58 +0100

    Revert "Vue3: layout fix + homepage basic structure"
    This reverts commit 47bad19c65b24312426a82fae921c653843f6b11.

--- frontend/vue3/src/routes.ts
+++ frontend/vue3/src/routes.ts
@@ -2,18 +2,12 @@ import type { RouteRecordRaw } from 'vue-router'
 
 import Home from '@components/home/HomePage.vue'
 import Login from '@components/auth/Login.vue'
-import Attributions from '@components/home/Attributions.vue'
 
 export const routes: RouteRecordRaw[] = [
     {
         path: '/',
         name: 'Home',
         component: Home,
-    },
-    {
-        path: '/attributions',
-        name: 'Attributions',
-        component: Attributions,
     },
     {
         path: '/login',
