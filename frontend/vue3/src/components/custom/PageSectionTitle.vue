4fecb5d0d072a3749cfddb733c392731bd2382b1
simjanos-dev <simjanos.dev@gmail.com>
2025-10-26 15:56:02 +0100

    Vue3: custom header/title component

---
+++ frontend/vue3/src/components/custom/PageSectionTitle.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+type Props = {
+    title: string
+}
+
+const { title } = defineProps<Props>()
+</script>
+
+<template>
+    <UPageHeader
+        :title="title"
+        :ui="{
+            root: 'py-2 border-0',
+            title: 'text-xl sm:text-xl',
+        }"
+    />
+</template>
