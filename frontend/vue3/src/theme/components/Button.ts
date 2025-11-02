6fe15a4b4047eb9a7821b00930bbedf6612e994d
simjanos-dev <simjanos.dev@gmail.com>
2025-11-03 00:58:49 +0100

    Vue3: add more spacing for buttons

--- frontend/vue3/src/theme/components/Button.ts
+++ frontend/vue3/src/theme/components/Button.ts
@@ -2,4 +2,23 @@ export default {
     slots: {
         base: 'rounded-full',
     },
+    variants: {
+        size: {
+            xs: {
+                base: 'px-3',
+            },
+            sm: {
+                base: 'px-3.5',
+            },
+            md: {
+                base: 'px-3.5',
+            },
+            lg: {
+                base: 'px-4',
+            },
+            xl: {
+                base: 'px-4',
+            },
+        },
+    },
 }
