aaed5edda2cf9ec639c8a714ff58ccea7458b8d3
simjandev <simjanos.dev@gmail.com>
2026-02-28 11:14:20 +0100

    add: new neutral colors

--- frontend/vue3/src/App.vue
+++ frontend/vue3/src/App.vue
@@ -4,18 +4,33 @@ import { onMounted } from 'vue'
 onMounted(() => {
     // temp code for setting the theme
     const neutralColors = {
-        50: '#dadadd',
-        100: '#cfced3',
-        200: '#bbbbc1',
-        300: '#a8a8b0',
-        400: '#96959f',
-        500: '#84828e',
-        600: '#72717e',
-        700: '#615f6d',
-        800: '#504f5c',
-        900: '#403f4a',
-        950: '#393842',
+    //     50: '#dadadd',
+    //     100: '#cfced3',
+    //     200: '#bbbbc1',
+    //     300: '#a8a8b0',
+    //     400: '#96959f',
+    //     500: '#84828e',
+    //     600: '#72717e',
+    //     700: '#615f6d',
+    //     800: '#504f5c',
+    //     900: '#403f4a',
+    //     950: '#393842',
+
+        50:  "#f6f6f7",
+        100: "#ededee",
+        200: "#dadadb",
+        300: "#c3c3c5",
+        400: "#a6a6a8",
+        500: "#89898a",
+        600: "#6d6d6f",
+        700: "#575759",
+        800: "#444446",
+        900: "#383839",
+        950: "#2d2d2f",
     }
+
+
+
     for (const [key, value] of Object.entries(neutralColors)) {
         document.documentElement.style.setProperty(`--ui-color-neutral-${key}`, value)
     }
